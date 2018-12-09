33884892f004bc0d0d9951dcc1df753cf3a5d47f
Trenton Potter <trpotter72@gmail.com>
2018-12-09 01:54:59 -0600

    Making an autocomplete function to implement later

---
+++ LilProblems/Trie.py
@@ -0,0 +1,79 @@
+from collections import deque
+
+class TrieNode:
+
+    def __init__(self):
+        self.alphabet = dict()
+        self.terminal = False
+        self.data = 0
+
+    def keyExists(self, char):
+        return char in self.alphabet
+
+
+class Trie:
+
+    def __init__(self):
+        self.root = TrieNode()
+
+    def search(self, string):
+        temp = self.searchNode(string)
+        if  temp != False:
+            return temp.data
+        else:
+            return False
+
+    def insert(self, string, data):
+        current = self.root
+        for char in string:
+            if current.keyExists(char): #Existing node
+                current = current.alphabet[char]
+            else: #Create a new node
+                current.alphabet[char] = TrieNode()
+                current = current.alphabet[char]
+        if current.terminal:
+            return False
+        else:
+            current.terminal = True
+            current.data = data
+            return data
+
+    #NOTE: This implementation would create nodes dynamically on the way
+    #       to a failed path.  This would increase the size requirement with no
+    #       additional speed, but it helps us not have to redefine the method
+    #       for an autocomplete feature
+    def searchNode(self, string):
+        current = self.root
+        for char in string:
+            if current.keyExists(char):
+                current = current.alphabet[char]
+            else:
+                current.alphabet[char] = TrieNode()
+                current = current.alphabet[char]
+        return current
+
+
+    def returnClosestNodes(self, count, string):
+        #use BFS until we find enough values
+        closest = []
+        current = self.searchNode(string)
+        bfs = deque()
+        bfs.append(current)
+        while (len(bfs)!=0 and len(closest) < count):
+            current = bfs.popleft()
+            if current.terminal:
+                closest.append(current)
+            for key, node in current.alphabet.items():
+                bfs.append(node)
+        return closest
+
+    def returnClosest(self, count, string):
+        return [x.data for x in self.returnClosestNodes(count, string)]
+
+a = Trie()
+a.insert("T", "Og node")
+a.insert("Trent", "Casual")
+a.insert("Trenton", "The usu\'")
+a.insert("Tee-Rent", "My self given name")
+a.insert("The Main man", "what a name")
+a.insert("The end of the world", "As we know it")
